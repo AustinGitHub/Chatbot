7830b8bfe8f2465a0412022002923c5973727dd4
Heather Howard <42278926+hhoward3@users.noreply.github.com>
2018-10-13 14:24:41 -0400

    Update and rename MagpieRunner4.java to ChatbotRunner.java

--- ChatbotRunner.java
+++ ChatbotRunner.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class MagpieRunner4
+public class ChatbotRunner
 {
 
 	/**
-	 * Create a Magpie, give it user input, and print its replies.
+	 * Create a chatbot, give it user input, and print its replies.
 	 */
 	public static void main(String[] args)
 	{
-		Magpie4 maggie = new Magpie4();
+		Chatbot maggie = new Chatpot();
 		
 		System.out.println (maggie.getGreeting());
 		Scanner in = new Scanner (System.in);
